52cca7c1b89962ded2b66f8d280a1e8c383e3b7b
Tyler Lawson <lawsont2@cse.msu.edu>
2020-04-05 23:45:17 -0400

    halfway through intro

--- app/src/main/java/edu/msu/lawsont2/arewethereyetlawsont2/MainActivity.java
+++ app/src/main/java/edu/msu/lawsont2/arewethereyetlawsont2/MainActivity.java
@@ -1,14 +1,181 @@
 package edu.msu.lawsont2.arewethereyetlawsont2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TO = "to";
+    private final static String TOLAT = "tolat";
+    private final static String TOLONG = "tolong";
+
+    private LocationManager locationManager = null;
+
+    private double latitude = 0;
+    private double longitude = 0;
+    private boolean valid = false;
+
+    private double toLatitude = 0;
+    private double toLongitude = 0;
+    private String to = "";
+
+    private SharedPreferences settings = null;
+
+    private ActiveListener activeListener = new ActiveListener();
+
+    private class ActiveListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            onLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            registerListeners();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        to = settings.getString(TO, "2250 Engineering");
+
+        toLatitude = Double.parseDouble(settings.getString(TOLAT, "42.724303"));
+        toLongitude = Double.parseDouble(settings.getString(TOLONG, "-84.480507"));
+
+        // Get the location manager
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        // Force the screen to say on and bright
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Also, dont forget to add overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //   int[] grantResults)
+            // to handle the case where the user grants the permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
     }
+
+    /**
+     * Set all user interface components to the current state
+     */
+    private void setUI() {
+        TextView viewProvider = (TextView)findViewById(R.id.textTo);
+        viewProvider.setText(to);
+
+        TextView viewLatitude = (TextView)findViewById(R.id.textLatitude);
+        TextView viewLongitude = (TextView)findViewById(R.id.textLongitude);
+        TextView viewDistance = (TextView)findViewById(R.id.textDistance);
+
+        if (!valid) {
+            viewLatitude.setText("");
+            viewLongitude.setText("");
+            viewDistance.setText("");
+        } else {
+            viewLatitude.setText(String.format("%1$.6f", latitude));
+            viewLongitude.setText(String.format("%1$.6f", longitude));
+            float[] results = new float[1];
+            Location.distanceBetween(longitude, latitude, toLongitude, toLatitude, results);
+            viewDistance.setText(String.format("%1$6.1fm", results[0]));
+        }
+    }
+
+    /**
+     * Called when this application becomes foreground again.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView viewProvider = (TextView)findViewById(R.id.textProvider);
+        viewProvider.setText("");
+
+        setUI();
+        registerListeners();
+    }
+
+    /**
+     * Called when this application is no longer the foreground application.
+     */
+    @Override
+    protected void onPause() {
+        unregisterListeners();
+        super.onPause();
+    }
+
+    private void registerListeners() {
+        unregisterListeners();
+
+        // Create a Criteria object
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAltitudeRequired(true);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(false);
+
+        String bestAvailable = locationManager.getBestProvider(criteria, true);
+
+        if(bestAvailable != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.requestLocationUpdates(bestAvailable, 500, 1, activeListener);
+            TextView viewProvider = (TextView)findViewById(R.id.textProvider);
+            viewProvider.setText(bestAvailable);
+            Location location = locationManager.getLastKnownLocation(bestAvailable);
+            onLocation(location);
+        }
+    }
+
+    private void unregisterListeners() {
+        locationManager.removeUpdates(activeListener);
+    }
+
+    private void onLocation(Location location) {
+        if(location == null) {
+            return;
+        }
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        valid = true;
+
+        setUI();
+    }
+
 }
